0a773b672ad9cd01b43cc62b1f0b378f5b26ceb5
Wesley Vollmar <wcvollmar@marathonpetroleum.com>
2017-12-05 13:37:58 -0500

    Initial commit for kata starting point

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,88 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGame.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Assert
+            Assert.IsTrue(game.Score() == 0);
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Act
+            for (var i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            // Assert
+            Assert.IsTrue(game.Score() == 20);
+        }
+
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Act
+            game.Roll(5);
+            game.Roll(5); // Spare
+            game.Roll(3);
+            for (var i = 0; i < 17; i++)
+            {
+                game.Roll(0);
+            }
+
+            // Assert
+            Assert.IsTrue(game.Score() == 16);
+        }
+
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Act
+            game.Roll(10); // Strike
+            game.Roll(3);
+            game.Roll(4);
+            for (var i = 0; i < 16; i++)
+            {
+                game.Roll(0);
+            }
+
+            // Assert
+            Assert.IsTrue(game.Score() == 24);
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            // Arrange
+            var game = new Game();
+
+            // Act
+            for (var i = 0; i < 12; i++)
+            {
+                game.Roll(10);
+            }
+
+            // Assert
+            Assert.IsTrue(game.Score() == 300);
+        }
+    }
+}
---
+++ BowlingGame/Game.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Game
+    {
+        public Game()
+        {
+
+        }
+
+        public void Roll(int pins)
+        {
+            // TODO - Implement the bowling roll
+            throw new NotImplementedException();
+        }
+
+        public int Score()
+        {
+            // TODO - Implement the bowling score mechanics
+            throw new NotImplementedException();
+        }
+    }
+}
